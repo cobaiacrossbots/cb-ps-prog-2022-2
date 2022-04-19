302c46ea4b4af5ec2d854d1768efda1f46c95095
cobaiacrossbots <103973455+cobaiacrossbots@users.noreply.github.com>
2022-04-18 23:51:01 -0300

    committing files

---
+++ 111/main.cpp
@@ -0,0 +1,14 @@
+// Git add
+/* 
+git add Aluno.h, Departamento.h, Disciplina.h, ElAluno.h, ElDepartamento.h, ElDisciplina.h, ElUniversidade.h, ListaAlunos.h, ListaDisciplinas.h, ListaDepartamentos.h, ListaUniversidades.h, Pessoa.h, Principal.h, Professor.h, Universidade.h, Aluno.cpp, Departamento.cpp, Disciplina.cpp, ElAluno.cpp, ElDepartamento.cpp, ElDisciplina.cpp, ElUniversidade.cpp, ListaAlunos.cpp, ListaDisciplinas.cpp, ListaDepartamentos.cpp, ListaUniversidades.cpp, main.cpp, Pessoa.cpp, Principal.cpp, Professor.cpp, Universidade.cpp 
+*/
+
+
+#include "Principal.h"
+
+int main()
+{
+	Principal objetoPrincipal;
+
+	return 0;
+}
